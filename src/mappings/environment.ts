21e807a5ad54f90f7caf19d0f47598f3c21875eb
Samuel Briole <samuel.briole@gmail.com>
2022-10-13 15:40:09 +0200

    replace WETH by 00

--- src/mappings/environment.ts
+++ src/mappings/environment.ts
@@ -2,13 +2,13 @@
 //
 export const ADDRESS_ZERO: string = '0x0000000000000000000000000000000000000000'
 export const FACTORY_ADDRESS: string = '0x4Eb4445EBc238080307A576Cee6B82baf39D5658'
-export const WETH_ADDRESS: string = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
+export const WETH_ADDRESS: string = '0xa6aad04cd9a55881b4d10878fe035004bb36fc60' // 00 Address
 export const USDC_WETH_PAIR: string | null = null
 export const DAI_WETH_PAIR: string | null = null
 export const USDT_WETH_PAIR: string | null = null
 // token where amounts should contribute to tracked volume and liquidity
 export const WHITELIST: string[] = [
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6' // WETH
+  '0xa6aad04cd9a55881b4d10878fe035004bb36fc60' // 00
 ]
 
 // production
